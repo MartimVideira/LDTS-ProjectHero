f579813ef90895ccac61d1ed7c7e649553571127
Martim <mitram790@gmail.com>
2021-11-08 22:04:37 +0000

    Added movement finnishing point 7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,7 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
@@ -8,8 +10,11 @@ import com.googlecode.lanterna.terminal.Terminal;
 import java.io.IOException;
 
 public class Game {
-    Screen screen;
+    private Screen screen;
     Terminal terminal;
+    private int x =10;
+    private int y = 10;
+
     public Game() {
         try {
             TerminalSize terminalSize = new TerminalSize(200, 200);
@@ -34,15 +39,59 @@ public class Game {
 
         }
         screen.refresh();
+    }
+    private void draw() throws IOException{
+        screen.clear();
+        screen.setCharacter(x,y,TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+
+    }
+    private void processKey(KeyStroke key){
+
+        //H  left
+        if(key.getKeyType() == KeyType.Character){
+            char key_char = key.getCharacter();
+            switch(key_char){
+                //Left
+                case 'h':
+                case 'H':
+                    x--;
+                    break;
+                //Right
+                case 'l':
+                case 'L':
+                    x++;
+                    break;
+                //Down
+                case 'j':
+                case 'J':
+                    y++;
+                    break;
+                //UP
+                case 'k':
+                case 'K':
+                    y--;
+                    break;
+                default:
+                    break;
+            }
+        }
 
 
     }
     public void run(){
-        String class_apresentation = "This is my first true labs class and i'm using linux!";
-        try {
-            draw_name(class_apresentation, 0, 0, false);
-        }catch (IOException e){
-            e.printStackTrace();
+        boolean running = true;
+        KeyStroke key;
+        while(running){
+
+            try{
+                draw();
+                key = screen.readInput();
+                processKey(key);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+
         }
     }
 
